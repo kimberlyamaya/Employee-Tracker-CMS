68e9e52351cc8cfcd911498f4348b463fcfc11f0
kimberlyamaya <kimberly.kimbell89@gmail.com>
2021-12-25 21:42:55 -0800

    finished db and started on prompts and functions

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const inquirer = require('inquirer');
+const mysql2 = require("mysql2");
+const consoleTable = require("console.table");
+
+// Connect to database
+const con = mysql2.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "Chr1stm@s2021",
+      database: "tracker_db"
+    },
+    console.log("connected to tracker_db!")
+  );
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("?")
+})
 
 // main menu
 function mainMenu () {
@@ -14,9 +32,46 @@ function mainMenu () {
     ])
     .then(function (answers) {
         if (answers.mainMenu === "View all departments") {
-            console.log("view all departments was chosen!")
+            let query = "select * from department"
+            con.query(query, function(err, res) {
+                if(err) return err;
+
+                console.table(res);
+
+                mainMenu()
+            })
             // viewDepartments()
         }
+
+        // view all roles
+
+        // view all employees
+
+        if (answers.mainMenu === "Add a department") {
+            addDepartment()
+        }
+    })
+}
+
+function addDepartment () {
+    // use inquirer for more prompts
+    return inquirer
+    .prompt([
+    {
+    type: "input",
+    name: "addDepartment",
+    message: "Enter Department Name:"
+    }
+    ])
+    // pass answers as insert statments into table
+    .then(function (answers) {
+        let insertStatement = (`INSERT INTO department (name) VALUES ('${answers.addDepartment}')`)
+        con.query(insertStatement, function(err, res) {
+            if(err) return err;
+            console.log("department added");
+            // return to main menu
+            mainMenu()
+        })
     })
 }
 
